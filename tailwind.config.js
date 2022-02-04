2a8a563403140c161cb1416f2e980f658d040f32
bernini <bernini@inventati.org>
2022-02-04 22:55:09 +0100

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  content: ['./src/**/*.njk', './src/**/*.md',],
+  theme: {
+    fontFamily: {
+      'sans': ['IBM Plex Sans', 'ui-sans-serif', 'system-ui'],
+      'serif': ['Playfair Display', 'ui-serif', 'Georgia']
+    },
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
